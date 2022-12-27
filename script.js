d82dbdaa73b59fa2e3b2404b1a7dcd1ab003eb68
aliemre2023 <aliemrekaya239@gmail.com>
2022-12-27 22:58:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeImage(){
+    var image = document.getElementById("omaygat");
+    image.src = "visuals/06a.jpg";
+}
+function resetImage(){
+    var image = document.getElementById("omaygat");
+    image.src = "visuals/ummm.gif";
+}
